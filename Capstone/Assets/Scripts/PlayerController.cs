d342aca1fee8342de189e01c873313b38e9f37b3
Zhong <20201783@o365.hanbat.ac.kr>
2024-01-31 14:01:23 +0900

    FEAT : Jump and set rigidbody

--- Capstone/Assets/Scripts/PlayerController.cs
+++ Capstone/Assets/Scripts/PlayerController.cs
@@ -4,12 +4,18 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField]
-    float _speed = 10.0f;
+    Rigidbody _rigid;
+
+    [SerializeField] float _speed = 10.0f;
+    [SerializeField] float _jumpForce = 500.0f;
+    bool isJumping = false;
+    // float _maxSpeed = 20.0f;
 
     void Start()
     {
         Application.targetFrameRate = 60;   // 60FPS로 고정
+
+        _rigid = GetComponent<Rigidbody>();
 
         // KeyAction이 호출되면 구독할 함수
         Managers.Input.KeyAction -= OnKeyboard; // 다른 곳에서 이미 호출되었을 가능성이 있으므로 삭제
@@ -18,33 +24,57 @@ public class PlayerController : MonoBehaviour
 
     void Update()
     {
-        
+
     }
 
     void OnKeyboard()
     {
-        if (Input.GetKey(KeyCode.W))
+        // float speedX = Mathf.Abs(_rigid.velocity.x);
+        // float speedZ = Mathf.Abs(_rigid.velocity.z);
+        // 속도 제한
+        // if ((speedX < _maxSpeed) && (speedZ < _maxSpeed))
         {
-            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(Vector3.forward), 0.2f);
-            transform.position += Vector3.forward * Time.deltaTime * _speed;
+            // WASD 입력에 따른 움직임
+            if (Input.GetKey(KeyCode.W))
+            {
+                transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(Vector3.forward), 0.2f);
+                transform.position += Vector3.forward * Time.deltaTime * _speed;
+                // this._rigid.AddForce(Vector3.forward * _speed);
+            }
+    
+            if (Input.GetKey(KeyCode.S))
+            {
+                transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(Vector3.back), 0.2f);
+                transform.position += Vector3.back * Time.deltaTime * _speed;
+                // this._rigid.AddForce(Vector3.back * _speed);
+            }
+            
+            if (Input.GetKey(KeyCode.A))
+            {
+                transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(Vector3.left), 0.2f);
+                transform.position += Vector3.left * Time.deltaTime * _speed;
+                // this._rigid.AddForce(Vector3.left * _speed);
+            }
+    
+            if (Input.GetKey(KeyCode.D))
+            {
+                transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(Vector3.right), 0.2f);
+                transform.position += Vector3.right * Time.deltaTime * _speed;
+                // this._rigid.AddForce(Vector3.right * _speed);
+            }
         }
 
-        if (Input.GetKey(KeyCode.S))
+        // 점프
+        if (Input.GetKeyDown(KeyCode.Space) && !isJumping)
         {
-            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(Vector3.back), 0.2f);
-            transform.position += Vector3.back * Time.deltaTime * _speed;
+            _rigid.AddForce(Vector3.up * _jumpForce);
+            isJumping = true;
         }
-        
-        if (Input.GetKey(KeyCode.A))
-        {
-            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(Vector3.left), 0.2f);
-            transform.position += Vector3.left * Time.deltaTime * _speed;
-        }
+    }
 
-        if (Input.GetKey(KeyCode.D))
-        {
-            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(Vector3.right), 0.2f);
-            transform.position += Vector3.right * Time.deltaTime * _speed;
-        }
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Floor")
+            isJumping = false;
     }
 }
