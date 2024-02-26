21d59a714cec64d40e56e67e17ecde6473c796d9
nh-05k <nh923915@gmail.com>
2024-02-26 20:39:23 +0900

    aa
    aa

---
+++ Capstone/Assets/Utils/Define.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Define
+{ 
+    public enum CameraMode
+    {
+        SideView,
+    }
+
+    public enum Layer
+    {
+        Monster = 8,
+        Ground = 9,
+        Block = 10,
+    }
+
+}
