44f23d7ec4097ba3c2ee2f5336137c2f4913a9ea
Zhong <20201783@o365.hanbat.ac.kr>
2024-10-04 01:05:25 +0900

    난희 작업 임포트

--- Capstone/Assets/1_Scripts/Nanhee/TutorialInteraction.cs
+++ Capstone/Assets/1_Scripts/Nanhee/TutorialInteraction.cs
@@ -1,51 +1,36 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class TutorialInteraction : MonoBehaviour
 {
- 
     public GameObject TutorialUI1; // 상호작용 UI
     public GameObject TutorialUI2;
-
 
     void Start()
     {
         TutorialUI1.SetActive(false); // 처음에 UI를 비활성화
         TutorialUI2.SetActive(false);
-        
+
+        // Coroutine으로 시간 기반 UI 제어
+        StartCoroutine(ManageUI());
     }
 
-
-
-    void OnTriggerEnter(Collider other)
+    // UI 표시와 숨김을 시간에 따라 제어하는 Coroutine
+    IEnumerator ManageUI()
     {
-        // 태그를 확인하여 UI를 활성화합니다.
-        if (other.CompareTag("Trigger1"))
-        {
-            TutorialUI1.SetActive(true);
-        }
-        else if (other.CompareTag("Trigger2"))
-        {
-            TutorialUI2.SetActive(true);
-        }
+        // 2초 대기 후 TutorialUI1 표시
+        yield return new WaitForSeconds(2f);
+        TutorialUI1.SetActive(true);
+        Debug.Log("TutorialUI1 Activated");
+
+        // 8초 대기 후 TutorialUI1 숨김 (2초~10초 동안 활성화)
+        yield return new WaitForSeconds(8f);
+        TutorialUI1.SetActive(false);
+        Debug.Log("TutorialUI1 Deactivated");
+
+        // 10초 대기 후 TutorialUI2 표시 (20초부터 TutorialUI2 활성화)
+        yield return new WaitForSeconds(10f);
+        TutorialUI2.SetActive(true);
+        Debug.Log("TutorialUI2 Activated");
     }
-
-    void OnTriggerExit(Collider other)
-    {
-
-        if (other.CompareTag("Trigger1"))
-        {
-            TutorialUI1.SetActive(false);
-        }
-        else if (other.CompareTag("Trigger2"))
-        {
-            TutorialUI2.SetActive(false);
-        }
-    }
-
-    
-    }
-
-
-
+}
