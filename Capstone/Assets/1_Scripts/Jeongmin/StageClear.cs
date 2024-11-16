08dcb9f4abfdffcbbad2d1d31b51ffbf71ec8f00
dreamerschoiminjun <128562816+dreamerschoiminjun@users.noreply.github.com>
2024-11-16 20:07:33 +0900

    sound
    sound

--- Capstone/Assets/1_Scripts/Jeongmin/StageClear.cs
+++ Capstone/Assets/1_Scripts/Jeongmin/StageClear.cs
@@ -10,14 +10,16 @@ public class StageClear : MonoBehaviour
     public bool stage1clear = false;
     public bool stage2clear = false;
     public bool stage3clear = false;
-    
+
     public Animator[] _door1;
     public Animator[] _door2;
     public Animator[] _door3;
 
     public GameObject _portal1;
     public GameObject _portal2;
-    
+
+    public AudioClip doorReverbClip; // Door_Reverb_v1_wav 오디오 클립
+
     void Awake()
     {
         photonView = GetComponent<PhotonView>();
@@ -47,15 +49,26 @@ public class StageClear : MonoBehaviour
             case 1:
                 _door1[0].SetBool("isClear", true);
                 _door1[1].SetBool("isClear", true);
+                PlayDoorSound(); // 사운드 재생
                 break;
             case 2:
                 _door2[0].SetBool("isClear", true);
                 _door2[1].SetBool("isClear", true);
+                PlayDoorSound(); // 사운드 재생
                 break;
             case 3:
                 _door3[0].SetBool("isClear", true);
                 _door3[1].SetBool("isClear", true);
+                PlayDoorSound(); // 사운드 재생
                 break;
+        }
+    }
+
+    void PlayDoorSound()
+    {
+        if (doorReverbClip != null)
+        {
+            AudioSource.PlayClipAtPoint(doorReverbClip, transform.position); // 오디오 클립 재생
         }
     }
 }
